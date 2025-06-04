62136e6c2b32f2efa677e8b21590763d0e5cdc3c
kaustubh850 <kaustubhrobotic@gmail.com>
2025-06-04 13:17:17 +0530

    Add files via upload

---
+++ electronics-lessons.js
@@ -0,0 +1,13 @@
+export const lessons = [
+    {
+      title: "What is a Transistor?",
+      content: "A transistor controls current in circuits.",
+      image: "transistor.png"
+    },
+    {
+      title: "What is a Resistor?",
+      content: "A resistor limits current in a circuit.",
+      image: "resistor.png"
+    }
+  ];
+  
