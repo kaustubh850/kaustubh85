315825eebfb561daa7459f6e423841518729b679
kaustubh850 <kaustubhrobotic@gmail.com>
2025-06-04 10:52:03 +0530

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,26 @@
+const quizzes = {
+    quiz1: [
+      {
+        question: "What is 2 + 2?",
+        options: ["3", "4"],
+        correctAnswer: "4"
+      },
+      {
+        question: "What is 3 × 3?",
+        options: ["6", "9"],
+        correctAnswer: "9"
+      }
+    ],
+    quiz2: [
+      {
+        question: "What is 5 + 5?",
+        options: ["9", "10"],
+        correctAnswer: "10"
+      },
+      {
+        question: "What is 8 ÷ 2?",
+        options: ["2", "4"],
+        correctAnswer: "4"
+      }
+    ]
+  };
