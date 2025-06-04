d4337bdea2dd16dce9a04a420b1fc137fdefb348
kaustubh850 <kaustubhrobotic@gmail.com>
2025-06-04 13:27:27 +0530

    Add files via upload

---
+++ programming-lessons.js
@@ -0,0 +1,13 @@
+
+export const lessons = [
+  {
+    title: "Introduction to Programming",
+    content: "Programming is the act of writing instructions for a computer to follow.",
+    image: "programming_intro.png"
+  },
+  {
+    title: "What is a Variable?",
+    content: "A variable is a storage location paired with a name which contains data that can be changed.",
+    image: "variable.png"
+  }
+];
