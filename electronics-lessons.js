618c83a7f2852d7156de23d4bfb1a00305c4f46b
kaustubh850 <kaustubhrobotic@gmail.com>
2025-06-04 14:35:20 +0530

    Update electronics-lessons.js

--- electronics-lessons.js
+++ electronics-lessons.js
@@ -2,12 +2,12 @@ export const lessons = [
     {
       title: "What is a Transistor?",
       content: "A transistor controls current in circuits.",
-      image: "transistor.png"
+     
     },
     {
       title: "What is a Resistor?",
       content: "A resistor limits current in a circuit.",
-      image: "resistor.png"
+     
     }
   ];
   
