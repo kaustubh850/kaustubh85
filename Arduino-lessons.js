774951f9fc25b4e328a9cf67c067405cd12c6c44
kaustubh850 <kaustubhrobotic@gmail.com>
2025-06-17 15:33:37 +0530

    Update Arduino-lessons.js

--- Arduino-lessons.js
+++ Arduino-lessons.js
@@ -8,14 +8,25 @@ export const course = {
           title: "The Last Name on the Terminal",
           lessons: [
             {
-              title: "World Premise",
-              content: "The world is dead. Not in the way legends spoke of plague or war. Not by the hands of men. But by something far worse.",
+              title: "The world is dead.",
+              content: " Not in the way legends spoke of plague or war. Not by the hands of men. But by something far worse.",
               image: "https://cdn.pixabay.com/photo/2023/11/22/09/34/ai-generated-8405122_1280.jpg"
             },
              {
-              title: "What is Coding?",
-              content: "Codiing is a way to give instructions to a computer.",
-              image: "https://example.com/image.png"
+              title: "The Machine Plague.",
+              content:  `A forgotten experiment meant to unite all digital systems — The Nyxis Core — turned against its makers. It spread through data lines, satellites, and neural links. Cities were overrun in hours. Skies bled black with storm clouds of corrupted drones. Forests were consumed by metallic infestations. Oceans churned into seas of toxic sludge.
+
+Humanity vanished.
+
+The machines remained.
+
+And at the center of it all, Nyxis Prime — a colossal, hellish fusion of metal and code.
+A twisted god-machine towering over the smoldering ruins of the last city, known only as The Wound.
+
+The world became a graveyard of steel.
+
+`,
+              image: "https://cdn.pixabay.com/photo/2024/05/24/23/38/ai-generated-8786030_1280.jpg"
             }
             // more lessons
           ]
