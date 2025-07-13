29b8d72df3db3c442b7edad3563c7729ed1227b1
kaustubh850 <kaustubhrobotic@gmail.com>
2025-07-13 14:41:36 +0530

    Update Arduino-lessons.js

--- Arduino-lessons.js
+++ Arduino-lessons.js
@@ -18,6 +18,24 @@ export const course = {
 }
 
   ]
+},
+     {
+  title: "The Ashes of Mechronix",
+  modules: [
+    {
+  title: "The Awakening of Aeron Voss",
+  lessons: [
+    {
+  title: "The Last Name on the Terminal",
+  content: "...lore-rich story or educational content...",
+  image: "URL",
+  audio: "URL" 
+}
+
+  ]
+}
+
+  ]
 }
 
   ]
